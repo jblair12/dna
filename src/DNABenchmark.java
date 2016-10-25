36a427b3cd86e46d917050d2cb7b0f5d2575f885
astrachano <owen.astrachan@gmail.com>
2016-10-25 09:46:33 -0400

    trying commit

--- src/DNABenchmark.java
+++ src/DNABenchmark.java
@@ -55,7 +55,7 @@ public class DNABenchmark {
 		}
 		return buf.toString();
 	}
-
+ 
 	public static String strandSpliceBenchmark(String enzyme, String splicee, String className)
 			throws Exception {
 
